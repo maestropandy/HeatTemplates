ea0c361476363ec38d77cea0f35613a52b0cee24
Mayank Kapoor <mayankkapoormail@gmail.com>
2019-05-29 16:01:56 +0000

    Test condition

--- telegraf_install.sh
+++ telegraf_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Telegraf install script
 
-echo "Hello World!"
+if [awk -F= '/^NAME/{print $2}' /etc/os-release = "Ubuntu"]
+then  
+  echo "It'sa me, Ubuntu"
+elif [awk -F= '/^NAME/{print $2}' /etc/os-release = "CentOS Linux"]
+then
+  echo "It's CentOS"
+else
+  echo "Not ubuntu or CentOS"
+fi
